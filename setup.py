3ee748c6e28c03ae9a58420a25ab280a55f41e27
René Vollmer <Aypac@users.noreply.github.com>
2020-06-09 10:35:58 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SetupVariableTracker',
+    version='0.0.1',
+    packages=['tabulate', 'datetime', 'codecs'],
+    url='https://github.com/Aypac/SetupVariableTracker',
+    license='MIT',
+    author='René Vollmer',
+    author_email='SetupVariableTracker@aypac.de',
+    description='Small library to track and log the declaration of new (setup) variables.'
+)
