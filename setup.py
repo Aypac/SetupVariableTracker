5fe6fcb9241749223ff26363bf4093903891d3fe
René Vollmer <Aypac@users.noreply.github.com>
2021-01-20 16:21:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name='SetupVariableTracker',
       # packages=find_packages(exclude=["*.tests", "tests"]),
       # packages=find_packages(),
       packages=['SetupVariableTracker', ],
-      install_requires=['tabulate', 'datetime', ],
+      install_requires=['tabulate', ],
       test_suite='SetupVariableTracker.tests',
       zip_safe=False,
       )
